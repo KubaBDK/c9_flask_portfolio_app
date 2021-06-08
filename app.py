6e58d73e09d93cd7799cafb8ea7bbb34be01eec4
KubaBDK <84875807+KubaBDK@users.noreply.github.com>
2021-06-08 07:32:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name=str(name) = "likes to eat eggs"
+	new_name=str(name) + "likes to eat eggs"
 	return render_template('index.html', name=new_name)
 
 
